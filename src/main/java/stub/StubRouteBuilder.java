f5909a68774491eb37787a9c63b172ab6e777ebf
Rouan Wilsenach <rouanw@gmail.com>
2014-01-29 10:08:54 +0200

    redirect activemq messages to file

--- src/main/java/stub/StubRouteBuilder.java
+++ src/main/java/stub/StubRouteBuilder.java
@@ -1,11 +1,16 @@
 package stub;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 
 public class StubRouteBuilder extends RouteBuilder {
 
     public void configure() {
-        from("file:src/data?noop=true")
-                .to("file:target/messages/uk");
+        CamelContext camelContext = new DefaultCamelContext();
+        camelContext.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616"));
+        from("activemq:queue:messages")
+                .to("file:target/messages");
     }
 }
